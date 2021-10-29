c7e21cc3c26b04d61fe7c8d611b82e4b9b884cf1
8Ren8 <lirenng95@1utar.my>
2021-10-29 20:46:04 +0800

    Added articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,6 @@
 class ArticlesController < ApplicationController
-
+    def show
+        #adding @ will turn a variable into instance variable that could be used on other pages
+        @article = Article.find(params[:id])
+    end
 end
