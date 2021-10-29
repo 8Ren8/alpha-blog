2c82a0d7b3aa4b6ddd85a65ebb23858e65170f69
8Ren8 <lirenng95@1utar.my>
2021-10-29 21:01:42 +0800

    Added listing of articles page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,9 @@ class ArticlesController < ApplicationController
         #adding @ will turn a variable into instance variable that could be used on other pages
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
+    
 end
