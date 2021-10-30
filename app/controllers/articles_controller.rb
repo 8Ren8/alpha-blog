c239802db410cb12f40cdafefe7f0a5271b5cb73
8Ren8 <lirenng95@1utar.my>
2021-10-30 11:38:05 +0800

    Added create article function including validation and notification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,24 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
-    
+
+    def create
+        #Strong parameters - whitelisting of data (values associated with attributes) that are received through the params hash. 
+        #During this process for articles you had to 'whitelist' the data coming through for the title and description fields.
+        @article = Article.new(params.require(:article).permit(:title, :description))
+
+        if @article.save
+            flash[:notice] = "New article was created successfully."
+            redirect_to article_path(@article)
+
+        else
+            render 'new'
+        end
+
+    end
+
+    def new
+        @article = Article.new
+    end
+
 end
