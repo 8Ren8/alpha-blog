6e224f86271203feb122ac92590c8faece13f42f
8Ren8 <lirenng95@1utar.my>
2021-10-30 12:08:07 +0800

    Added edit and update function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
     def create
         #Strong parameters - whitelisting of data (values associated with attributes) that are received through the params hash. 
         #During this process for articles you had to 'whitelist' the data coming through for the title and description fields.
@@ -23,8 +27,20 @@ class ArticlesController < ApplicationController
 
     end
 
-    def new
-        @article = Article.new
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+
+        else
+            render 'edit'
+        end
+
     end
 
 end
