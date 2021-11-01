3bc8884771dd35f3e0a56a9f8bd5ef728429d0e3
8Ren8 <lirenng95@1utar.my>
2021-11-01 16:45:21 +0800

    Added CRUD operations and navigation links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only:[:show, :edit, :update, :destroy]
+
     def show
-        #adding @ will turn a variable into instance variable that could be used on other pages
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
 
     def index
+        #adding @ will turn a variable into instance variable that could be used on other pages
         @articles = Article.all
     end
 
@@ -13,9 +15,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        #Strong parameters - whitelisting of data (values associated with attributes) that are received through the params hash. 
-        #During this process for articles you had to 'whitelist' the data coming through for the title and description fields.
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
 
         if @article.save
             flash[:notice] = "New article was created successfully."
@@ -28,12 +28,12 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        #@article = Article.find(params[:id])
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
 
@@ -41,6 +41,25 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
 
+    end
+
+    def destroy
+        #@article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    #anything below private is a private method, which is avaialble in this controller only
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        #Strong parameters - whitelisting of data (values associated with attributes) that are received through the params hash. 
+        #During this process for articles you had to 'whitelist' the data coming through for the title and description fields.
+        params.require(:article).permit(:title, :description)
     end
 
 end
