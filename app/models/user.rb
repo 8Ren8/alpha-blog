4dc48fa581ce7ea6a9c261a3d34fce58dd13e2ea
8Ren8 <lirenng95@1utar.my>
2021-11-09 21:43:03 +0800

    Added password field to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase if email }
     has_many :articles
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     VALID_EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
@@ -6,4 +7,6 @@ class User < ApplicationRecord
         uniqueness: { case_sensitive: false }, 
         format:{ with:VALID_EMAIL_REGEX, multiline:true }, 
         length: { maximum: 80 }
+    
+    has_secure_password
 end
