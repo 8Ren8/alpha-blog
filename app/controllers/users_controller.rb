4cb8fcfe0c40ca5ac4c929cf2bc3b9c7f163a59b
8Ren8 <lirenng95@1utar.my>
2021-11-11 01:30:32 +0800

    Added user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,15 @@ class UsersController < ApplicationController
         @articles = @user.articles
     end
 
+    def index
+        @user = User.all
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Welcome #{@user.username}, You have signed up successfully."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'new'
         end
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if  @user.update(user_params)
             flash[:notice] = "Your profile had been updated successfully."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
