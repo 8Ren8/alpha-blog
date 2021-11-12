8956b2e98037209f94a92f5f164fdd41aab652ec
8Ren8 <lirenng95@1utar.my>
2021-11-12 12:05:07 +0800

    Added log in and log out session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,23 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:sessions][:email].downcase)
+        if user && user.authenticate(params[:sessions][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged In Successfully"
+            redirect_to user
+
+        else
+            flash.now[:alert] = "There was some error in your login details"
+            render 'new'
+
+        end
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged Out Successfully"
+        redirect_to root_path
     end
 
 end
