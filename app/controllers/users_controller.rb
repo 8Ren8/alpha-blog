52eea8e9724d17c8e03958ba2b7964024e8cadd4
8Ren8 <lirenng95@1utar.my>
2021-11-12 12:57:33 +0800

    Added authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
     def new
         @user = User.new
     end
 
     def show
-        @user = User.find(params[:id])
+        #set up before_action method so no need repeat again in other methods
+        #@user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -15,6 +18,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome #{@user.username}, You have signed up successfully."
             redirect_to @user
         else
@@ -23,11 +27,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+        #@user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
+        #@user = User.find(params[:id])
         if  @user.update(user_params)
             flash[:notice] = "Your profile had been updated successfully."
             redirect_to @user
@@ -39,5 +43,9 @@ class UsersController < ApplicationController
     private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
